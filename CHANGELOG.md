3142dfad02104d5157da4e444f88616028475dbe
Ayo <ayo@metatheory.gg>
2024-09-07 19:43:17 +0800

    Use Unity's built-in `[Shortcut]` attribute for shortcut key assignments.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,11 @@
-﻿## [1.0.1] - 2024-09-07
-### Update
-- Add Blender Scene View Control For Unity Scene View
+﻿## [1.0.2] - 2024-09-07
+### Updated
+- Use Unity's built-in `[Shortcut]` attribute for shortcut key assignments.
+
+## [1.0.1] - 2024-09-07
+### Updated
+- Minor Refactor.
 
 ## [1.0.0] - 2024-09-07
-### First Release
-- Add Blender Scene View Control For Unity Scene View
+### Initial Release
+- Added Blender Scene View Control for Unity Scene View management.
--- Editor/BlenderSceneViewControl.cs
+++ Editor/BlenderSceneViewControl.cs
@@ -1,74 +1,77 @@
 using UnityEditor;
+using UnityEditor.ShortcutManagement;
 using UnityEngine;
 
 namespace UnityAyoExtensions.Editor
 {
     public static class BlenderSceneViewControl
     {
-        private static readonly Quaternion DefaultDirection = Quaternion.Euler(30, 45, 0);
+        private static readonly Quaternion DefaultDirection = Quaternion.Euler(30, -135, 0);
 
-        [MenuItem("Edit/Blender Scene View Control/Front View (Numpad 1)")]
+        [Shortcut("BlenderSceneViewControl/Front View", KeyCode.Keypad1, displayName = "Blender Scene View Control/Front View")]
         private static void SetFrontView()
         {
             RotateSceneViewTo(SceneView.lastActiveSceneView, Quaternion.Euler(0, 0, 0));
         }
 
-        [MenuItem("Edit/Blender Scene View Control/Right View (Numpad 3)")]
+        [Shortcut("BlenderSceneViewControl/Right View", KeyCode.Keypad3, displayName = "Blender Scene View Control/Right View")]
         private static void SetRightView()
         {
             RotateSceneViewTo(SceneView.lastActiveSceneView, Quaternion.Euler(0, 90, 0));
         }
 
-        [MenuItem("Edit/Blender Scene View Control/Top View (Numpad 7)")]
+        [Shortcut("BlenderSceneViewControl/Top View", KeyCode.Keypad7, displayName = "Blender Scene View Control/Top View")]
         private static void SetTopView()
         {
             RotateSceneViewTo(SceneView.lastActiveSceneView, Quaternion.Euler(90, 0, 0));
         }
 
-        [MenuItem("Edit/Blender Scene View Control/Bottom View (Numpad 9)")]
+        [Shortcut("BlenderSceneViewControl/Bottom View", KeyCode.Keypad9, displayName = "Blender Scene View Control/Bottom View")]
         private static void SetBottomView()
         {
             RotateSceneViewTo(SceneView.lastActiveSceneView, Quaternion.Euler(-90, 0, 0));
         }
 
-        [MenuItem("Edit/Blender Scene View Control/Rotate Down (Numpad 2)")]
+        [Shortcut("BlenderSceneViewControl/Rotate Down", KeyCode.Keypad2, displayName = "Blender Scene View Control/Rotate Down")]
         private static void RotateDown()
         {
             RotateSceneView(SceneView.lastActiveSceneView, Vector3.right, -15);
         }
 
-        [MenuItem("Edit/Blender Scene View Control/Rotate Up (Numpad 8)")]
+        [Shortcut("BlenderSceneViewControl/Rotate Up", KeyCode.Keypad8, displayName = "Blender Scene View Control/Rotate Up")]
         private static void RotateUp()
         {
             RotateSceneView(SceneView.lastActiveSceneView, Vector3.right, 15);
         }
 
-        [MenuItem("Edit/Blender Scene View Control/Rotate Left (Numpad 4)")]
+        [Shortcut("BlenderSceneViewControl/Rotate Left", KeyCode.Keypad4, displayName = "Blender Scene View Control/Rotate Left")]
         private static void RotateLeft()
         {
             RotateSceneView(SceneView.lastActiveSceneView, Vector3.up, -15);
         }
 
-        [MenuItem("Edit/Blender Scene View Control/Rotate Right (Numpad 6)")]
+        [Shortcut("BlenderSceneViewControl/Rotate Right", KeyCode.Keypad6, displayName = "Blender Scene View Control/Rotate Right")]
         private static void RotateRight()
         {
             RotateSceneView(SceneView.lastActiveSceneView, Vector3.up, 15);
         }
 
-        [MenuItem("Edit/Blender Scene View Control/Toggle Orthographic (Numpad 5)")]
+        [Shortcut("BlenderSceneViewControl/Toggle Orthographic", KeyCode.Keypad5, displayName = "Blender Scene View Control/Toggle Orthographic")]
         private static void ToggleOrthographic()
         {
             SceneView sceneView = SceneView.lastActiveSceneView;
             if (sceneView == null) return;
+
             sceneView.orthographic = !sceneView.orthographic;
             sceneView.Repaint();
         }
 
-        [MenuItem("Edit/Blender Scene View Control/Reset View (Numpad 0)")]
+        [Shortcut("BlenderSceneViewControl/Reset View", KeyCode.Keypad0, displayName = "Blender Scene View Control/Reset View")]
         private static void ResetSceneViewRotation()
         {
             SceneView sceneView = SceneView.lastActiveSceneView;
             if (sceneView == null) return;
+
             sceneView.LookAt(sceneView.pivot, DefaultDirection);
             sceneView.orthographic = false; // Set to perspective view
             sceneView.Repaint();
@@ -77,6 +80,7 @@ namespace UnityAyoExtensions.Editor
         private static void RotateSceneViewTo(SceneView sceneView, Quaternion rotation)
         {
             if (sceneView == null) return;
+
             sceneView.LookAt(sceneView.pivot, rotation);
             sceneView.orthographic = true;
             sceneView.Repaint();
@@ -85,6 +89,7 @@ namespace UnityAyoExtensions.Editor
         private static void RotateSceneView(SceneView sceneView, Vector3 axis, float angle)
         {
             if (sceneView == null) return;
+
             sceneView.rotation *= Quaternion.AngleAxis(angle, axis);
             sceneView.Repaint();
         }
